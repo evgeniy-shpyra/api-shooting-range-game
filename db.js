dd29c4dc7879e215b8433bd3dcdb26e179f22b45
Evgeniy <56407183+jekamix258@users.noreply.github.com>
2022-05-20 12:41:33 +0300

    conect to database2

--- db.js
+++ db.js
@@ -7,13 +7,12 @@ const Pull = require('pg').Pool
 //     database: 'score'
 // })
 
-const pool = new Pull({
-    user: "fsejrumiiqrnqd",
-    password: 'e3495f777d3ba02c6f80440f0108d046929a1ab87ffe2ec91a2c4a6b1443fe59',
-    host: 'ec2-54-165-90-230.compute-1.amazonaws.com',
-    port: 5432,
-    database: 'ddistg39ngseo3'
-})
+const pool =  Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  })
 
 
 
