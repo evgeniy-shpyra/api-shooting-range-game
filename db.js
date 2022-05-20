1de4884154f329460633008c564835385d8ec4b8
Evgeniy <56407183+jekamix258@users.noreply.github.com>
2022-05-20 12:48:36 +0300

    conect to database3

--- db.js
+++ db.js
@@ -7,12 +7,23 @@ const Pull = require('pg').Pool
 //     database: 'score'
 // })
 
-const pool =  Pool({
+const pool = Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
-      rejectUnauthorized: false
+        rejectUnauthorized: false
     }
-  })
+}).get('/db', async (req, res) => {
+        try {
+            const client = await pool.connect();
+            const result = await client.query('SELECT * FROM test_table');
+            const results = { 'results': (result) ? result.rows : null };
+            res.render('pages/db', results);
+            client.release();
+        } catch (err) {
+            console.error(err);
+            res.send("Error " + err);
+        }
+    })
 
 
 
